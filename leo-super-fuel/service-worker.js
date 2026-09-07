4e60bbf473707bd06a2296ed85268d336acd6623
zkhansari73-ui <z.khansari73@gmail.com>
2026-09-07 13:30:26 +0330

    Add Leo Super Fuel service-worker.js

---
+++ leo-super-fuel/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'leo-super-fuel-en-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys
+        .filter(key => key.startsWith('leo-super-fuel-en-') && key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      if (response && response.ok && new URL(event.request.url).origin === self.location.origin) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
